a67e0d5ceef0e8a0ebdd05773b4e70f4ba529273
fochka <fochee@mail.ru>
2023-01-24 21:14:49 +0400

    FeedMer fix request

---
+++ docs/precache-manifest.3c5eb33becf971d6788770da3b497563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "480b2bd483669c490295",
+    "url": "/static/css/main.5b54720c.chunk.css"
+  },
+  {
+    "revision": "480b2bd483669c490295",
+    "url": "/static/js/main.89e81528.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f4d1cf738634d7579e4",
+    "url": "/static/js/2.2455313b.chunk.js"
+  },
+  {
+    "revision": "a207b85cf57cd8e6aeb1b54f521a7c4f",
+    "url": "/index.html"
+  }
+];
